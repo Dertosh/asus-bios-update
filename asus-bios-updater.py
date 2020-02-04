1f98820cec0124b864b6bb4a00c438a2c9462acd
Aleksandr Miyadin <dertosh7@gmail.com>
2020-02-04 15:37:49 +0300

    code refaktoring

--- asus-bios-updater.py
+++ asus-bios-updater.py
@@ -1,27 +1,34 @@
 from urllib.request import urlopen
-import os 
+import os
 import json
 import re
 import datetime
 
-def get_html(url):
+
+def get_latest_bios_info(model_name: str):
+    """ 
+    Get information about latest bios from asus.com for model.
+    model_name -- model of matherboard
+    """
+    cpu = ""
+    url = "https://www.asus.com/support/api/product.asmx/GetPDBIOS?website=global&model=" + \
+        model_name + "&cpu="+cpu+"&callback=supportpdpage"
     rs = urlopen(url)
     text = rs.readline().decode('ascii')
     json_text = re.findall(r"\((.*?)\)", text)[0]
     result = json.loads(json_text)
-  
+
     latest_bios = result["Result"]["Obj"][0]["Files"][0]
-    print(latest_bios)
-    
-    newBIOSreleaseDate = datetime.datetime.strptime(latest_bios["ReleaseDate"], '%Y/%m/%d')
-    newBIOSversion = int(latest_bios["Version"])
+    return latest_bios
 
-    currentBIOSreleaseDate = datetime.datetime.strptime(os.popen('dmidecode -s bios-release-date').read()[:-1],'%m/%d/%Y')
-    currentModel,currentBIOSversion = (os.popen('dmidecode -s bios-version').read()[:-1]).split('.')
-    currentBIOSversion = int(currentBIOSversion)
 
-    if newBIOSreleaseDate <= currentBIOSreleaseDate or newBIOSversion <= currentBIOSversion:
-        return
+def bios_update(model: str, latest_bios_info, efi_path: str):
+    """
+    Downloading bios from asus.com and unzip to efi path.
+    model -- model of matherboard
+    latest_bios_info -- information about latest bios
+    efi_path -- path to efi folder
+    """
     print("Update!\n")
     print("Current BIOS")
     print("Release date: " + currentBIOSreleaseDate.strftime('%x'))
@@ -29,17 +36,26 @@ def get_html(url):
     print("\nNew BIOS")
     print("Release date: " + newBIOSreleaseDate.strftime('%x'))
     print("Version: " + str(newBIOSversion))
+    os.system('cd ' + efi_path)
+    os.system('wget ' + latest_bios_info["DownloadUrl"]["Global"])
+    os.system("unzip "+model+'*' +
+              latest_bios_info["Version"]+".zip -d " + EFI_path)
+    os.system("rm " + model+'*'+latest_bios_info["Version"]+".zip")
 
-    os.system('cd /boot/EFI/')
-    os.system('wget ' + latest_bios["DownloadUrl"]["Global"])
-    os.system("unzip "+currentModel+'*'+str(newBIOSversion)+".zip -d " + EFI_path)
-    os.system("rm " +currentModel+'*'+str(newBIOSversion)+".zip")
 
-
-model_name = "UX333FA"
-cpu = ""
 EFI_path = "/boot/EFI/"
 
-url = "https://www.asus.com/support/api/product.asmx/GetPDBIOS?website=global&pdhashedid=YXAdDPC8x8xmgSYE&model=" + model_name + "&cpu="+cpu+"&callback=supportpdpage"
-print(url)
-get_html(url)
+currentBIOSreleaseDate = datetime.datetime.strptime(
+    os.popen('dmidecode -s bios-release-date').read()[:-1], '%m/%d/%Y')
+currentModel, currentBIOSversion = (
+    os.popen('dmidecode -s bios-version').read()[:-1]).split('.')
+currentBIOSversion = int(currentBIOSversion)
+
+latest_bios_info = get_latest_bios_info(currentModel)
+
+newBIOSreleaseDate = datetime.datetime.strptime(
+    latest_bios_info["ReleaseDate"], '%Y/%m/%d')
+newBIOSversion = int(latest_bios_info["Version"])
+
+if newBIOSreleaseDate > currentBIOSreleaseDate and newBIOSversion > currentBIOSversion:
+    bios_update(currentModel, latest_bios_info, EFI_path)
